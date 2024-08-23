4d068178cbb79a94fe1dcea30e0f0774bec8f85c
Kenechukwu Akubue <kengoon19@gmail.com>
2024-08-23 22:25:33 +0100

    Design HomeScreen

---
+++ Model/home_screen.py
@@ -0,0 +1,8 @@
+from Model.base_model import BaseScreenModel
+
+
+class HomeScreenModel(BaseScreenModel):
+    """
+    Implements the logic of the
+    :class:`~View.HomeScreen.home_screen.HomeScreenView` class.
+    """
