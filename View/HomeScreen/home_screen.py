3f79ffde9319ee640622bc57ac6e7a7b05255502
Kenechukwu Akubue <kengoon19@gmail.com>
2024-09-06 14:51:53 +0100

    update home screen design

--- View/HomeScreen/home_screen.py
+++ View/HomeScreen/home_screen.py
@@ -1,4 +1,5 @@
 from View.base_screen import BaseScreenView
+from kivymd.utils.set_bars_colors import set_bars_colors
 
 
 class HomeScreenView(BaseScreenView):
@@ -8,3 +9,17 @@ class HomeScreenView(BaseScreenView):
         The view in this method tracks these changes and updates the UI
         according to these changes.
         """
+
+    def on_enter(self, *args):
+        set_bars_colors(
+            self.theme_cls.surfaceColor,
+            self.theme_cls.surfaceContainerHighestColor,
+            "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+        )
+
+    def on_leave(self, *args):
+        set_bars_colors(
+            self.theme_cls.surfaceColor,
+            self.theme_cls.surfaceColor,
+            "Dark" if self.theme_cls.theme_style == "Light" else "Light"
+        )
