8617d7891d61b855bc6a2113faebce4710dfedcb
Kenechukwu Akubue <kengoon19@gmail.com>
2024-09-06 14:57:43 +0100

    Update screens

--- View/screens.py
+++ View/screens.py
@@ -10,8 +10,31 @@ from View.HomeScreen.home_screen import HomeScreenView
 from Model.yield_prediction_screen import YieldPredictionScreenModel
 from Controller.yield_prediction_screen import YieldPredictionScreenController
 from View.YieldPredictionScreen.yield_prediction_screen import YieldPredictionScreenView
+from Model.crop_analysis_screen import CropAnalysisScreenModel
+from Controller.crop_analysis_screen import CropAnalysisScreenController
+from View.CropAnalysisScreen.crop_analysis_screen import CropAnalysisScreenView
+from Model.inventory_screen import InventoryScreenModel
+from Controller.inventory_screen import InventoryScreenController
+from View.InventoryScreen.inventory_screen import InventoryScreenView
+from Model.chat_screen import ChatScreenModel
+from Controller.chat_screen import ChatScreenController
+from View.ChatScreen.chat_screen import ChatScreenView
 
 screens = {
+    'crop analysis screen': {
+        'model': CropAnalysisScreenModel,
+        'controller': CropAnalysisScreenController,
+        'view': CropAnalysisScreenView,
+        'kv': "./View/CropAnalysisScreen/crop_analysis_screen.kv"
+    },
+
+    'inventory screen': {
+        'model': InventoryScreenModel,
+        'controller': InventoryScreenController,
+        'view': InventoryScreenView,
+        'kv': "./View/InventoryScreen/inventory_screen.kv"
+    },
+
     'yield prediction screen': {
         'model': YieldPredictionScreenModel,
         'controller': YieldPredictionScreenController,
@@ -31,6 +54,13 @@ screens = {
         'controller': HomeScreenController,
         'view': HomeScreenView,
         'kv': "./View/HomeScreen/home_screen.kv"
+    },
+
+    'chat screen': {
+        'model': ChatScreenModel,
+        'controller': ChatScreenController,
+        'view': ChatScreenView,
+        'kv': "./View/ChatScreen/chat_screen.kv"
     },
 
 }
